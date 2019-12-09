4d30e0e49922f93063d58fe9ce4c17e4cb904ccd
lautaro <lautarovallelopez@gmail.com>
2019-12-09 13:50:59 -0300

    migracion de tabla friendships, falta terminar

---
+++ database/migrations/2019_12_09_164049_create_friendships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFriendshipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friendships', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->integer('user1_id')->unsigned();
+            $table->integer('user2_id')->unsigned();
+            $table->enum('status',['pending','confirmed','blocked']);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friendships');
+    }
+}
